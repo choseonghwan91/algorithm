65c7f3d7b90e59b1cefafaec450a4c8d588774c7
Seonghwan Cho <82944360+choseonghwan91@users.noreply.github.com>
2023-06-05 22:27:23 +0900

    [level 1] Title: 최소직사각형, Time: 7.08 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.swift
@@ -1,19 +1,12 @@
 import Foundation
 
 func solution(_ sizes:[[Int]]) -> Int {
-    var maxX = 0
-    var maxY = 0
-    
-    sizes.forEach {
-        let max = $0.max()!
-        let min = $0.min()!
-        if max > maxX {
-            maxX = max
-        }
-        if min > maxY {
-            maxY = min
-        }
+    var maxNum = 0
+    var minNum = 0
+
+    for size in sizes {
+        maxNum = max(maxNum, size.max()!)
+        minNum = max(minNum, size.min()!)
     }
-    
-    return maxX * maxY
+    return maxNum * minNum
 }
