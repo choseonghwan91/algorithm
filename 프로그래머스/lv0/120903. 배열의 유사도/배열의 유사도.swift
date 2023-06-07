7869e684e1c50fb020bb29d62e1d453562a684ef
Seonghwan Cho <82944360+choseonghwan91@users.noreply.github.com>
2023-06-07 13:09:20 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ s1:[String], _ s2:[String]) -> Int {
+    var answer = 0
+    
+    for i in s1 {
+        for j in s2 {
+            if i == j {
+                answer += 1
+            }
+        }
+    }
+    
+    return answer
+}
