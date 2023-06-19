0e801955d342d715cd2892e1da437acb88425e87
Seonghwan Cho <82944360+choseonghwan91@users.noreply.github.com>
2023-06-20 08:49:35 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.swift
@@ -0,0 +1,22 @@
+func solution(_ arr:[Int]) -> Int {
+    let LCM = arr.reduce(arr[0], { a, b in getLCM(a, b) })
+    return LCM
+}
+
+func getLCM (_ a: Int, _ b: Int) -> Int {
+    var GCD = Int.max
+    var preNum = a
+    var currNum = b
+    
+    while currNum != 0 {
+        let leftNum = preNum % currNum
+        if  leftNum != 0 {
+            preNum = currNum
+            currNum = leftNum
+        } else {
+            GCD = currNum
+            currNum = leftNum
+        }
+    }
+    return (a * b) / GCD
+}
