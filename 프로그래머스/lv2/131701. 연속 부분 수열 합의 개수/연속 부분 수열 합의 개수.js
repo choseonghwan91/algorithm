424182cfee71f525a5180f1f96b5f1ea13b720f0
Seonghwan Cho <82944360+choseonghwan91@users.noreply.github.com>
2023-07-22 21:23:14 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 80.65 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,14 +1,14 @@
 function solution(elements) {
-    let arr = [];
+    let set = new Set();
     let elementsRepeat = [...elements, ...elements];
     
     elements.forEach((_,idx) => {
         let num = 0;
         for(let i = idx; i < elements.length + idx ; i++){
-            arr.push(num + elementsRepeat[i])
+            set.add(num + elementsRepeat[i])
             num += elementsRepeat[i]
         }
     })
     
-    return [...new Set(arr)].length;
+    return set.size;
 }
