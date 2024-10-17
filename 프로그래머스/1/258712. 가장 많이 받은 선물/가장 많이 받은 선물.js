cf32f34ebc6f05a0e27cb7b58cbf09cd861814cb
Seonghwan Cho <82944360+choseonghwan91@users.noreply.github.com>
2024-10-17 15:56:29 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 15.98 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,48 @@
+function solution(friends, gifts) {
+    const reciveList = friends.map(()=> 0);
+    const score = getScore(friends, gifts);
+    const graph = Array.from({length: friends.length}, () => Array.from({length: friends.length}, () => 0));
+    const friendsIndex = getFriendsIndex(friends);
+    
+    gifts.forEach((gift) => {
+        const [giver, reciver] = gift.split(" ");
+        graph[friendsIndex[giver]][friendsIndex[reciver]]++;
+    })
+    
+    for(let i = 0; i < friends.length; i++){
+        for(let j = i; j < friends.length; j++) {
+            if(i === j) continue;
+            if(graph[i][j] > graph[j][i]) reciveList[i]++;
+            if(graph[i][j] < graph[j][i]) reciveList[j]++;
+            if(graph[i][j] === graph[j][i]) {
+                if(score[friends[i]] > score[friends[j]]) reciveList[i]++;
+                if(score[friends[i]] < score[friends[j]]) reciveList[j]++;
+            }
+        }
+    }
+    
+    return Math.max(...reciveList)
+}
+
+const getFriendsIndex = (friends) => {
+    const _friends = {};
+    friends.forEach((friend, i) => {
+        _friends[friend] = i;
+    })
+    return _friends;
+}
+
+const getScore = (friends, gifts) => {
+    const _friends = {};
+    friends.forEach((friend) => {
+        _friends[friend] = 0;
+    })
+    
+    gifts.forEach((gift) => {
+        const [giver, reciver] = gift.split(" ");
+        _friends[giver]++;
+        _friends[reciver]--;
+    })
+    
+    return _friends;
+}
